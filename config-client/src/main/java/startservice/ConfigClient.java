1f3f9597a4287c6a6e1505a7c85ace255fa87d5b
DanikWRT <danil23004@gmail.com>
2024-02-26 20:06:28 +0700

    config-client

--- config-client/src/main/java/startservice/ConfigClient.java
+++ config-client/src/main/java/startservice/ConfigClient.java
@@ -1,15 +1,12 @@
-package com.example.testreactive;
-
-
+package startservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
-public class TestReactiveApp {
+@EnableEurekaClient
+public class ConfigClient {
     public static void main(String[] args) {
-        SpringApplication.run(TestReactiveApp.class, args);
+        SpringApplication.run(ConfigClient.class, args);
     }
 }
